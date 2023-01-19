e521740e715e7d52e2fcc0cc528f4ae0dae66864
Aldcoder <122543592+Aldcoder@users.noreply.github.com>
2023-01-19 09:49:23 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    led.plotBarGraph(
+    0,
+    5
+    )
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Hello!")
+})
+input.onGesture(Gesture.Shake, function () {
+    music.setBuiltInSpeakerEnabled(true)
+    music.playMelody("D E D E F E F E ", 300)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showArrow(ArrowNames.North)
+})
+basic.showLeds(`
+    # . . . #
+    # . # . #
+    # . # . #
+    # . # . #
+    . # # # .
+    `)
